fc71e222020abdc73a928555d513c6f73f624724
ulydev <ulydev@live.fr>
2017-02-15 12:06:41 +0100

    fixed audio

--- server/conf.lua
+++ server/conf.lua
@@ -1,9 +1,15 @@
 function love.conf(t)
   
+  --disable all unnecessary modules
   t.modules.joystick = false
+  t.modules.keyboard = false
+  t.modules.mouse = false
+  t.modules.touch = false
   t.modules.physics = false
   t.modules.graphics = false
   t.modules.audio = false
+  t.modules.font = false
+  t.modules.video = false
 
   t.window = nil
   
